5cb507ba873e3272b002ed260b90b83b104f0ca0
berperly <135066562+berperly@users.noreply.github.com>
2025-05-07 15:50:58 -0300

    Create senha.vue

---
+++ senha.vue
@@ -0,0 +1,23 @@
+// Esse código impõe um minimo de 6 caracteres para uma senha
+
+
+<template>
+    <input type="password" v-model="senha" placeholder="Digite sua senha" />
+<p v-if="senha.length < 6">Sua senha deve conter no mínimo 6 caracteres</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      senha: '',
+    }
+  }
+}
+</script>
+
+<style>
+input{
+  border-radius: 5px;
+}
+</style>
