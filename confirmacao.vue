5875d71776f401eb8b811a9c1c513a61db87c84e
berperly <135066562+berperly@users.noreply.github.com>
2025-05-07 16:19:54 -0300

    Create confirmacao.vue
    confirma um alert ao ler email e senha

---
+++ confirmacao.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="email" v-model="mail" placeholder="Digite seu email" />
+    <br />
+    <input type="password" v-model="senha" placeholder="Digite sua senha" />
+    <br />
+    <button @click="enviarMensagem" :disabled="!dadosValidos">Realizar cadastro</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mail: '',
+      senha: '',
+    }
+  },
+  computed: {
+    dadosValidos() {
+      return this.mail && this.senha
+    }
+  },
+  methods:{
+     enviarMensagem() {
+    alert("Seu cadastro foi realizado com sucesso!");
+    },
+    },
+}
+</script>
+
+<style>
+input, button {
+  border-radius: 5px;
+  background-color: white;
+}
+</style>
