ef346302e05104edae28e639b6b925d735c50ef7
berperly <135066562+berperly@users.noreply.github.com>
2025-05-07 15:42:55 -0300

    Create ola.vue

---
+++ ola.vue
@@ -0,0 +1,23 @@
+// Esse código retorna o nome escrito no input com uma mensagem "Olá (nome), seja bem vindo!"
+
+
+<template>
+    <input type="text" v-model="nome" placeholder="Digite seu nome" />
+    <p v-if="nome">Bem-vindo, {{ nome }}!</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: ''
+    }
+  }
+}
+</script>
+
+<style>
+input{
+  border-radius: 5px;
+}
+</style>
